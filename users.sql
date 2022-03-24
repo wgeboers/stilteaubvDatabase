e4309089517514f979621b0167e7442d3810d56d
Bart Frijters <bjalfrij@avans.nl>
2022-03-24 11:20:26 +0100

    code voor het maken van user accounts

---
+++ users.sql
@@ -0,0 +1,20 @@
+CREATE USER 'Admin'@'%' IDENTIFIED BY 'AdminPassword'
+GRANT ALL PRIVILEGES ON stilteaubv.* TO 'Admin'@'%' WITH GRANT OPTION;
+
+CREATE USER 'SalesPerson'@'%' IDENTIFIED BY 'SalesPassword'
+GRANT SELECT, INSERT, UPDATE, DELETE ON stilteaubv.Orders TO 'Sales'@'%' WITH GRANT OPTION;
+GRANT SELECT, INSERT, UPDATE ON stilteaubv.Users TO 'Sales'@'%' WITH GRANT OPTION;
+GRANT SELECT, INSERT, UPDATE, DELETE ON stilteaubv.Products TO 'Sales'@'%' WITH GRANT OPTION;
+
+CREATE USER 'Production'@'%' IDENTIFIED BY 'ProductionPassword';
+GRANT SELECT, INSERT, UPDATE, DELETE ON stilteaubv.Products TO 'Production'@'%' WITH GRANT OPTION;
+
+CREATE USER 'Customer'@'%' IDENTIFIED BY 'CustomerPassword';
+GRANT SELECT ON stilteaubv.orderheaders TO 'Customer'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+GRANT UPDATE, SELECT ON stilteaubv.users to 'Customer'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+GRANT SELECT, INSERT ON stilteaubv.orderlines TO 'Customer'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
